efd2995aac72c8082793443892c41b345eb13b67
Rob Gaston <robcoach0221@gmail.com>
2017-02-17 15:23:40 -0500

    add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-        id: 1,
-        description: 'Meet mom for lunch',
-        completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    complete: false
-}, {
-    id: 3,
-    description: 'Get oil changed',
-    complete: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 //  GET request /todos
 app.get('/todos', function (req, res) {
@@ -35,8 +28,15 @@ app.get('/todos/:id', function (req, res) {
     }   else {
        res.status('404').send();
     }
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    body.id = todoNextId++;
+    todos.push(body);
     
-//    res.send('Asking for todo with if of ' + req.params.id); 
+    res.json(body);
 });
 
 app.get('/', function (req, res) {
